3aab3f6937f228888f4456002e98571f84884b4e
Gonza <gonza.cunet@outlook.com>
2024-05-29 11:47:09 -0300

    prueba off deploy 2.0

--- Client/rtdb.ts
+++ Client/rtdb.ts
@@ -1,7 +1,9 @@
 import firebase from "firebase";
 
 export const API_BASE_URL =
-  process.env.ENVIRONMENT == "DEV" ? "http://localhost:3000" : "";
+  process.env.ENVIRONMENT == "DEV"
+    ? "http://localhost:3000"
+    : "https://pptonline-z6eq.onrender.com";
 
 firebase.initializeApp({
   apiKey: "AIzaSyACD-PFhB7oZMNpkDK2JOxc0Rsqwdk_Pc8",
